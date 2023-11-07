53c9fa683c7976262d63f3bd688dde7e2db11006
Rahul B  Nabde <GS-3715@GSLAB.COM>
2023-11-07 10:59:50 +0530

    UI files for the app

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,11 +1,26 @@
 from app.main import blueprint
-from flask import render_template
+from flask import render_template, request, redirect
 from config import Config
 from app.utils.watsonHelper import WatsonHelper
 
 @blueprint.route('/')
+@blueprint.route('/login', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    msg=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin':
+            msg = f'Welcome to index page {username}'
+            return redirect("/chat")
+        else:
+            msg = 'Incorrect UserName or Password'
+    return render_template('login.html',msg=msg)
+
+@blueprint.route('/chat')
+def user_Interaction():
+    return render_template("index.html")
+
 
 @blueprint.route('/wat/')
 def test_page():
