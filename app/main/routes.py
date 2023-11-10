4188ce1276d9208251e9e626fb373bf16105d47f
shripadsuradkar <shripad.suradkar@gslab.com>
2023-11-10 14:56:12 +0530

    Add REST API for execute gcloud cmds

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,12 +1,24 @@
 from app.main import blueprint
-from flask import render_template
+from flask import render_template, request,jsonify
 from config import Config
 from app.utils.watsonHelper import WatsonHelper
+from app.utils.scripts import executeCommands
+
 
 @blueprint.route('/')
 def index():
     return render_template('index.html')
 
+
 @blueprint.route('/wat/')
 def test_page():
     return '<br> URL : ' + Config.WATSONX_URL + '<br> API KEY : ' + Config.WATSONX_API_KEY + '<br> PROJECT_ID : ' + Config.WATSONX_PROJECT_ID + '<br> TOKEN : ' + WatsonHelper().get_token()
+
+
+@blueprint.route('/executeCommands', methods=['GET', 'POST'])
+def execute_commands():
+    data = request.get_json()
+    command = data["Command"]
+    KEY_FILE = data["Key_File"]
+    result = executeCommands(command, KEY_FILE)
+    return jsonify(result)
---
+++ app/utils/scripts.py
@@ -0,0 +1,136 @@
+import subprocess
+import json
+import shlex
+
+
+def listInstances(vmName, projectId):
+    try:
+        list = "gcloud compute instances list --filter=\"name=(\'{}\')\"".format(
+            vmName)
+        instance_list_command = list+" --format json --project " + projectId
+        instance_output_josn = json.loads(
+            subprocess.check_output(shlex.split(instance_list_command)))
+        print("--\n")
+        is_exist = len(instance_output_josn)
+        print(is_exist)
+        return is_exist
+        # print(instance_output_josn)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        return{ "status": 500,"error":"Not able to list instances"}
+
+
+
+def executeCommands(commands, KEY_FILE):
+    result = {"status": 200, "response": ""}
+    try:
+        # load a key_file
+        data = json.load(open(KEY_FILE))
+        projectId = str(data['project_id'])
+
+        # login
+        login = loginWithServiceAccount(KEY_FILE)
+        if login == False:
+            result = {"status": 401, "response": "Unauthorized Access"}
+            return result
+
+        # execute commands
+        if "instances delete" in commands:
+            print("Delete Block--------", commands)
+            vmName = splitCommand(commands)
+            is_vm_exist = listInstances(vmName, projectId)
+            if is_vm_exist != 0:
+                execute_command = commands + \
+                    " --format json --zone us-east1-c -q --project " + projectId
+                commands_delete_josn = json.loads(
+                    subprocess.check_output(shlex.split(execute_command)))
+                print(commands_delete_josn)
+                result['response'] = "Virtual Machine Deleted Successfully"
+                # return result
+            else:
+                print("No Virtual Machine avaialble with {} name".format(vmName))
+                allInstance = listallInstances(projectId)
+                result['response'] = allInstance
+                # return result
+
+        else:
+            print("Excuting comand: ", commands)
+            execute_command = commands + " --format json --project " + projectId
+
+            if "instances create" in commands:
+                execute_command = commands + " --format json --zone us-east1-c --project " + projectId
+            commands_output_josn = json.loads(
+                subprocess.check_output(shlex.split(execute_command)))
+            print(commands_output_josn)
+            result['response'] = commands_output_josn
+            # return commands_output_josn
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        result['response'] = e.returncode
+        result['status'] = 500
+        # return result
+
+    except Exception as e:
+        print("Error", e)
+        result['response'] = e.returncode
+        result['status'] = 500
+        # return result
+    finally:
+        logout(projectId)
+        print(result)
+        return result
+
+
+def splitCommand(command):
+    splitdata = command.split()
+    checkIndex = splitdata.index('delete')
+    value = splitdata[checkIndex+1]
+    print(value)
+    return value
+
+# # list all running instances
+
+
+def listallInstances(projectId):
+    try:
+        instance_list_command = "gcloud compute instances list --format json --project " + projectId
+        instance_output_josn = json.loads(
+            subprocess.check_output(shlex.split(instance_list_command)))
+        print("--\n")
+        vms = []
+        # print(instance_output_josn)
+        for instance in instance_output_josn:
+            print(instance['name'])
+            vms.append(instance['name'])
+        return vms
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        return{ "status": 500,"error":"Not able to List Insances"}
+
+
+
+def loginWithServiceAccount(KEY_FILE):
+    try:
+        projectID = json.load(open(KEY_FILE))
+        gcloud_login__command = "gcloud auth login --cred-file={} --format json".format(
+            KEY_FILE)
+        gcloud_login_josn = json.loads(
+            subprocess.check_output(shlex.split(gcloud_login__command)))
+        # print(gcloud_login_josn)
+        return True
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code {e.returncode}")
+        return{ "status": 500,"error":"Not able to login"}
+
+
+
+def logout(projectId):
+    try:
+        logout_command = "gcloud auth revoke --project "+projectId
+        logout = json.loads(subprocess.check_output(
+            shlex.split(logout_command)))
+        return {"msg":"Logout Successfully"}
+    except Exception as e:
+        # print(f"Command failed with return code {e.returncode}")
+        print("Error while logging out")
+        return{"status":500,"error":"Not able to logout"}
